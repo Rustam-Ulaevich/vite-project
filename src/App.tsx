98f64b13fd8134f06e9d193435d55b6170d43f43
Rustam-Ulaevich <praporglamur@gmail.com>
2026-01-12 16:04:47 +0300

    change app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,11 @@ export function App() {
     console.log('effect')
     fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
       headers: {
-        'api-key': 'be0d9c95-8751-4645-a84e-146c0f9b7f67'
+        'api-key': 'be0d9c95-8751-4645-a84e-146c0f9b7f67'                    // удалил 7
       }
     }).then(res => res.json())
-        .then(json => setTracks(json.data))
-  }, [])
-
- 
+    .then(json => setTracks(json.data))
+  }, []) 
     
   if(tracks === null) {
     return <div>
@@ -23,12 +21,14 @@ export function App() {
     </div>
   }
 
-  // if (tracks.length === 0) {
-  //     return <div>
-  //       <h1>Musicfun</h1>
-  //       <span>No tracks</span>
-  //     </div>
-  // }
+  if (tracks.length === 0) {
+      return <div>
+        <h1>Musicfun</h1>
+        <span>No tracks</span>
+      </div>
+  }
+
+  let selectedTrack = tracks.find( track => track.id === selectedTrackId)
 
   return (
     <div>
@@ -36,24 +36,33 @@ export function App() {
       <button onClick = { () => {setSelectedTrackId(null)}}>
         Reset selection
       </button>
-      <ul>
-        {
-          tracks.map((track) => {
-            return (
-              <li key={track.id} style={{
-                border: track.id === selectedTrackId ? '1px solid yellow' : 'none'
-              }}>
-                <div onClick={ () => {
-                  setSelectedTrackId(track.id)
+      <div style={{display: "flex", gap: "30px"}}>
+        <ul>
+          {            
+            tracks.map((track) => {
+              return (
+                <li key={track.id} style={{
+                  border: track.id === selectedTrackId ? '1px solid yellow' : 'none'
                 }}>
-                  {track.attributes.title}
-                </div>
-                <audio src={track.attachments[0].url} controls></audio>
-              </li>
-            )
-          })
-        }
-      </ul>
+                  <div onClick={ () => {
+                  setSelectedTrackId(track.id)
+                  }}>
+                   {track.attributes.title}
+                  </div>
+                  <audio src={track.attributes.attachments[0].url} controls></audio>
+                </li>
+              )            
+            })
+          }
+        </ul>
+
+        <div>
+          <h3>Details</h3>
+          {selectedTrackId === null 
+            ? 'Track is not selected' 
+            : selectedTrack.attributes.title}
+        </div>
+      </div>      
     </div>
   )
   
